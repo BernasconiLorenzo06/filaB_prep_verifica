6f329ae2a63ab9e25b18724adcc138a1be5b36fe
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2024-05-24 10:33:12 +0000

    punto1

---
+++ bernasconi/src/app/vegetables.model.ts
@@ -0,0 +1,12 @@
+export class vegetableList {
+    nome: string;
+    descrizione: string;
+    prezzo: string;
+
+
+    constructor(nome: string, descrizione:string, prezzo:string) {
+        this.nome = nome
+        this.descrizione = descrizione
+        this.prezzo = prezzo
+    }
+}
